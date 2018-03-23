002b03db0aca560f88bac8a7f0ab2d2045e0329d
Belir <patrykpeas@gmail.com>
2018-03-23 22:13:47 +0100

    Add Home view with basic functionality

--- app/scenes/Home/index.js
+++ app/scenes/Home/index.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { PureComponent } from 'react';
+import { View, Platform } from 'react-native';
+
+import SceneView from 'components/SceneView';
+import Input from 'components/Input';
+import Button from 'components/Button';
 
 import styles from './styles';
 
-class Home extends Component {
+class Home extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchPhrase: ''
+    };
+  }
+
+  _onChangeSearchInput = (text) => {
+    this.setState({
+      searchPhrase: text
+    });
+  }
+
+  _handleSearch = () => {
+
+  }
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text>
-          Home
-        </Text>
-      </SafeAreaView>
+      <SceneView align='center'>
+        <View style={styles.searchFormWrapper}>
+          <Input
+            onChangeText={this._onChangeSearchInput}
+            value={this.state.searchPhrase}
+            placeholder="Hmm... I'm looking for..."
+          />
+          <Button
+            onPress={this._handleSearch}
+            title="Search"
+            fullWidth={true}
+          />
+        </View>
+      </SceneView>
     )
   }
 }
--- app/scenes/Home/styles.js
+++ app/scenes/Home/styles.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
 
+import { BLUE } from 'constants/colors';
+
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  searchFormWrapper: {
+    alignSelf: 'stretch',
+    backgroundColor: BLUE,
+    borderRadius: 5,
+  }
 });
