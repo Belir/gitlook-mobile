3e85c6a91edcc25862b034c315dc640eb73cd26c
Belir <patrykpeas@gmail.com>
2018-03-25 15:29:16 +0200

    Create TableView component

---
+++ app/components/TableView/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Link from 'components/Link';
+import styles from './styles';
+
+const TableView = (props) => {
+  const {
+    titleText, cols
+  } = props;
+
+  return (
+    <View style={styles.row}>
+      <View
+        style={[
+          styles.col,
+          styles.titleCol,
+          props.titleWidth && {
+            width: props.titleWidth,
+            maxWidth: props.titleWidth,
+          }]}
+      >
+        <Text>
+          {titleText}
+        </Text>
+      </View>
+      {
+        props.cols.map((col) => (
+          <View key={col.key} style={styles.col}>
+            {
+              col.url ?
+                <Link
+                  to={col.url}
+                  text={col.text}
+                />
+              : <Text>
+                {col.text}
+              </Text>
+            }
+          </View>
+        ))
+      }
+    </View>
+  )
+};
+
+TableView.defaultProps = {
+  titleWidth: 80,
+}
+
+export default TableView;
---
+++ app/components/TableView/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    paddingBottom: 2,
+  },
+  titleCol: {
+    paddingRight: 10,
+  },
+  col: {
+    flex: 1,
+    flexDirection: 'row',
+    flexShrink: 1,
+  },
+});
